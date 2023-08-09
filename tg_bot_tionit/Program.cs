f4f64d834b60f9efb2dca7606be8dc898e589832
sevase <kotaseva8@gmail.com>
2023-08-09 15:26:57 +0300

    TIK-001: исправил баг с поломкой подключения. Отправка соо от клиента в группу воспроизводится

--- tg_bot_tionit/Program.cs
+++ tg_bot_tionit/Program.cs
@@ -15,10 +15,23 @@ namespace tg_bot_tionit
         // токен
         private static readonly TelegramBotClient Bot_Tg = new TelegramBotClient("6489394873:AAET3koh_wVNMO2rX7zIyFLwBYOoR70ZlFc"); 
         private static Dictionary<long, string> Our_Clients = new Dictionary<long, string>();// словарь с данными клиента
+        
+        
 
         static async Task Main(string[] args)
         {
+            // Подписка на событие получения новых сообщений
+            Bot_Tg.OnMessage += Input_Message;
 
+            // Начало приема новых сообщений от Telegram
+            Bot_Tg.StartReceiving();
+
+            // Вывод сообщения в консоль и ожидание нажатия клавиши для завершения программы
+            Console.WriteLine("Bot is listening. Press any key to exit...");
+            Console.ReadKey();
+
+            // Остановка приема новых сообщений от Telegram
+            Bot_Tg.StopReceiving();
         }
 
         private static async void Input_Message(object sender, MessageEventArgs e)
@@ -40,9 +53,21 @@ namespace tg_bot_tionit
             // создание темы + получение сообщения от клиента
             var clientTheme = $"Forwarded from {Our_Clients[mes.Chat.Id]}";
             var mesClient = $"{clientTheme}\n{mes.Text}";
+            // отправляю соо в группу
+            await SendInChatOperators(mesClient);
+
+            
 
 
 
+        }
+
+        private static async Task SendInChatOperators(string mesClient)
+        {
+            long IdGroup = -1001711439253; // это ID группы операторов, через него как я понял, можно будет отправить сообщение в группу
+                                           // получил я его с помощью бота LeadConverter
+
+            await Bot_Tg.SendTextMessageAsync(IdGroup, mesClient);
         }
 
 
