d5d05f1c75b64c02025c82bd7ca24371e9352235
sevase <kotaseva8@gmail.com>
2023-08-09 14:12:32 +0300

    Добавьте файлы проекта.

---
+++ tg_bot_tionit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot;
+
+namespace tg_bot_tionit
+{
+    internal class Program
+    {
+        // токен
+        private static readonly TelegramBotClient Bot_Tg = new TelegramBotClient("6489394873:AAET3koh_wVNMO2rX7zIyFLwBYOoR70ZlFc"); 
+        private static Dictionary<long, string> Our_Clients = new Dictionary<long, string>();// словарь с данными клиента
+
+        static async Task Main(string[] args)
+        {
+
+        }
+
+        private static async void Input_Message(object sender, MessageEventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+}
+   
